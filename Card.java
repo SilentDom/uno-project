1bee32e55189ea76b9b9967870d64591e3638aa8
Dom <92395872+SilentDom@users.noreply.github.com>
2021-10-12 18:49:56 -0500

    Add files via upload

--- Card.java
+++ Card.java
@@ -26,43 +26,26 @@ public class Card {
 		WildDrawFour;
 	}
 	
-	private String cardNumber;
-	private String cardType;
-	private String cardColor;
 	private Color[] colors = Color.values();
 	private Type[] types = Type.values();
+	private Color color;
+	private Type type;
 	
-	public Card(Color[] colors, Type[] types) {
-		this.colors = colors;
-		this.types = types;
-	}
-	
-	public String getCardNumber() {
-		return cardNumber;
-	}
-
-	public void setCardNumber(String cardNumber) {
-		this.cardNumber = cardNumber;
+	public Card(Color color, Type type) {
+		this.color = color;
+		this.type = type;
 	}
 	
-	public String getCardColor() {
-		return cardColor;
+	public Color getColor() {
+		return color;
 	}
-
-	public void setCardColor(String cardColor) {
-		this.cardColor = cardColor;
-	}
-
-	public void setCardType(String cardType) {
-		this.cardType = cardType;
-	}
-
-	public String getCardType() {
-		return cardType;
+	
+	public Type getType() {
+		return type;
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("Card Color: %s%nCard Number: %s%nCard Type: %s%n", cardColor, cardNumber, cardType);
+		return String.format("Card Color:%s%nCard Type:%s%n", color, type);
 	}
 }
