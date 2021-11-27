48a31b3d2bdb47e7e0e651223fd69b37cf4a428d
Dom <92395872+SilentDom@users.noreply.github.com>
2021-11-27 16:54:41 -0600

    Add files via upload

---
+++ MainMenu.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root =
+            FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+
+        Scene scene = new Scene(root); // attach scene graph to scene
+        stage.setTitle("UNO Main Menu"); // displayed in window's title bar
+        stage.setScene(scene); // attach scene to stage
+        stage.show(); // display the stage
+    }
+
+    public static void main(String[] args) {
+        // create a UnoGame object and call its start method
+        launch(args);
+    }
+}
